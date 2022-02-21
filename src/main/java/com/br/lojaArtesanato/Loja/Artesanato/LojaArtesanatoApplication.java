03d84b3e90a0d7d66f43f72fc1a9d22236d2078a
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-02-21 11:48:20 -0300

    Adiciona arquivos src

---
+++ src/main/java/com/br/lojaArtesanato/Loja/Artesanato/LojaArtesanatoApplication.java
@@ -0,0 +1,13 @@
+package com.br.lojaArtesanato.Loja.Artesanato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaArtesanatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaArtesanatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/lojaArtesanato/Loja/Artesanato/LojaArtesanatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lojaArtesanato.Loja.Artesanato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaArtesanatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
