68f85853ff571f525cfdab4b5a97b26d81604469
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-02-22 10:54:08 -0300

    Adiciona metodo para listar todos os produtos

--- src/main/java/com/br/lojaArtesanato/Loja/Artesanato/controller/ProdutoController.java
+++ src/main/java/com/br/lojaArtesanato/Loja/Artesanato/controller/ProdutoController.java
@@ -1,5 +1,27 @@
 package com.br.lojaArtesanato.Loja.Artesanato.controller;
 
-//aqui vai ser meu crud produto
+import com.br.lojaArtesanato.Loja.Artesanato.exception.ResourceNotFoundException;
+import com.br.lojaArtesanato.Loja.Artesanato.model.Produto;
+import com.br.lojaArtesanato.Loja.Artesanato.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:42000")
+@RequestMapping("/api/v1")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping("/produto")
+    public List<Produto> getAllProduto(){
+        return produtoRepository.findAll();
+    }
 }
