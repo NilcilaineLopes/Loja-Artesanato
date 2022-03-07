76ab9ee4fe6e0e47185ff316330e1777cf1aa224
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-03-07 15:30:11 -0300

    Renomea nome do package, adiciona @Date @Collumn

--- src/main/java/com/br/lojaArtesanato/Loja/Artesanato/entity/Venda.java
+++ src/main/java/com/br/lojaArtesanato/Loja/Artesanato/entity/Venda.java
@@ -1,32 +1,38 @@
-package com.br.lojaArtesanato.Loja.Artesanato.model;
+package com.br.lojaArtesanato.Loja.Artesanato.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity
+@Builder
+@Getter
+@Table(name = "vendas")
 public class Venda {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Integer qtd_de_ventas;
 
+    @Column(nullable = false)
     private Float valor;
 
+    @Column(nullable = false)
     private Date data_venda;
 
-    private Produto produto;
+    public Venda() {
+    }
 
-    private Cliente cliente;
-
-    public Venda(Long id, Integer qtd_de_ventas, Float valor, Date data_venda, Produto produto, Cliente cliente) {
+    public Venda(Long id, Integer qtd_de_ventas, Float valor, Date data_venda) {
         this.id = id;
         this.qtd_de_ventas = qtd_de_ventas;
         this.valor = valor;
         this.data_venda = data_venda;
-        this.produto = produto;
-        this.cliente = cliente;
-    }
-
-    public Venda() {
     }
 
     public Long getId() {
@@ -45,6 +51,10 @@ public class Venda {
         this.qtd_de_ventas = qtd_de_ventas;
     }
 
+    public Float getValor() {
+        return valor;
+    }
+
     public void setValor(Float valor) {
         this.valor = valor;
     }
@@ -55,47 +65,5 @@ public class Venda {
 
     public void setData_venda(Date data_venda) {
         this.data_venda = data_venda;
-    }
-
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    @Override
-    public String toString() {
-        return "Vendas{" +
-                "id=" + id +
-                ", qtd_de_ventas=" + qtd_de_ventas +
-                ", valor=" + valor +
-                ", data_venda=" + data_venda +
-                ", produto=" + produto +
-                ", cliente=" + cliente +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Venda)) return false;
-        Venda venda = (Venda) o;
-        return Objects.equals(id, venda.id) && Objects.equals(qtd_de_ventas, venda.qtd_de_ventas) && Objects.equals(valor, venda.valor) && Objects.equals(data_venda, venda.data_venda) && Objects.equals(produto, venda.produto) && Objects.equals(cliente, venda.cliente);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, qtd_de_ventas, valor, data_venda, produto, cliente);
     }
 }
