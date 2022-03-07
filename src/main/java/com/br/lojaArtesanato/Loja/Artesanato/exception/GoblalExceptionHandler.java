9578795dd39412706e4a6ac3ec7bd562b3526d0a
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-03-07 15:31:56 -0300

    Modifica HttpStatus

--- src/main/java/com/br/lojaArtesanato/Loja/Artesanato/exception/GoblalExceptionHandler.java
+++ src/main/java/com/br/lojaArtesanato/Loja/Artesanato/exception/GoblalExceptionHandler.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
 public class GoblalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
