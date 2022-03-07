54bea52d23596e59ca4cc66d0eb0c742d5e53433
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-03-07 15:37:17 -0300

    Adiciona um verificador de Status do controller

---
+++ src/main/java/com/br/lojaArtesanato/Loja/Artesanato/controller/StatusController.java
@@ -0,0 +1,12 @@
+package com.br.lojaArtesanato.Loja.Artesanato.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping(path = "/api/status")
+    public String check(){
+        return "online";
+    }
+}
